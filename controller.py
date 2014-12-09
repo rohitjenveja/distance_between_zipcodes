37f4c1c02732dbc404601828abc66d14b40d3ff4
rohitjenveja <rjenveja@gmail.com>
2014-12-08 21:28:19 -0800

    Cleaned up and actually got it working! Successfully displays a graph and all zip codes in between. Added more doc strings and cleaned up some style issues.

--- controller.py
+++ controller.py
@@ -1,34 +1,108 @@
 #!/usr/bin/python
-from flask import Flask
 
+"""The controller that renders HTML templates."""
+
+__author__ = ("Rohit Jenveja - rjenveja@gmail.com")
+
+from flask import Flask
+from flask import make_response
 from flask import render_template
 from flask import request, url_for
-from third_party.pygmaps import pygmaps as gmap
 
 import geo_helper
 
+import logging
+
+from pygeocoder import Geocoder
+import shapely
+from third_party.pygmaps import pygmaps as gmap
+
+
 application = Flask(__name__)
+
+
+class Error(Exception):
+  """Base error class from which all other exceptions inherit from."""
+
+class InvalidAddress(Error):
+  """Raised if a user enters an invalid address."""
+
 
 @application.route('/')
 def index():
   """Main handler that prompts user for departure and destination.
 
-  Valid GET arguments: departure and destination. Processes user 
-  request if all necessary information is available. 
-  """
-  departure = request.args.get('departure')
-  destination = request.args.get('destination')
+  Valid GET arguments:
+    departure: The full address from which to depart.
+    destination: The desired destination.
 
+  Processes user request if all necessary information is available.
+  If an invalid address is passed, HandleException is called and may
+  raise an exception or ask the user to try again.
+  """
+  departure, destination = (
+      request.args.get('departure'), request.args.get('destination'))
+  geo_data = {}
+  # Use pygeocoder to validate and get precise longitude and latitude.
   if departure and destination:
-    # User has filled out the form.
-    # Note: this data still needs to be validated/sanitized.
-    geo_helper.process_request(departure, destination)
+    departure = Geocoder.geocode(request.args.get('departure'))
+    destination = Geocoder.geocode(request.args.get('destination'))
+    logging.info("Depature: %s", departure)
+    logging.info("Destination: %s", destination)
+
+    if departure.valid_address and destination.valid_address:
+      # Process the request and return a dict to populate in to the template.
+      geo_data = {
+                  'departure': departure.formatted_address,
+                  'destination': destination.formatted_address,
+                  'departure_coordinates': departure.coordinates,
+                  'destination_coordinates': destination.coordinates,
+                 }
+      zips, image_path = geo_helper.ProcessRequest(
+          departure.coordinates,
+          destination.coordinates)
+      geo_data["zip_codes"] = zips
+      geo_data["image_path"] = image_path
+    else:
+      # If in dev mode, raise the exception. Otherwise, show form.html.
+      HandleException(InvalidAddress, "User has entered an invalid address.",
+                      'form.html')
+  # Prompt user with form
+  return render_template('form.html', geo_data=geo_data)
+
+
+@application.route("/images/<path:path>")
+def images(path):
+  """Loads images from local file system."""
+  fullpath = "./" + path
+  resp = make_response(open(fullpath).read())
+  resp.content_type = "image/png"
+  return resp
+
+
+def HandleException(exception_to_raise, error_reason, template):
+  """Logic to determine how to display the exception to end-user.
+
+  Args:
+    exception_to_raise: The exception class that will be raised.
+    error_reason: The text to pass in to the exception class or template.
+    template: The HTML text to display, if not in debug mode.
+
+  Returns:
+    A rendered template if we are not in debug mode.
+
+  Raises:
+    An exception that occurred earlier in the stack, if in debug mode.
+  """
+
+  if application.debug:
+    raise exception_to_raise(error_reason)
   else:
-    # Prompt user with form
-    return render_template('form.html')
+    render_template(template, error_happened=error_reason)
 
 
 if __name__ == '__main__':
+  # Load the GeoDataFrame in to memory prior to starting server.
   geo_helper.CreateGeoDataFrame()
   application.debug = True
   application.run(port=5000,host='0.0.0.0')
--- geo_helper.py
+++ geo_helper.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 
-# Needs to be put at the top, unless X display is specified.
+"""The geo_helper determines zip codes and plots points between two addresses.
+
+Uses geopandas to assemble data and compare with shapely LineString objects.
+"""
+
 import matplotlib
 matplotlib.use('Agg')
 
 import geopandas as gp
 import matplotlib.pyplot as plt
+
+import shapely
 
 ZIP_HEADER = 'ZCTA5CE10'
 DPI = 200
@@ -17,21 +23,44 @@ def CreateGeoDataFrame():
   global GDF
   if not GDF:
     GDF = gp.GeoDataFrame.from_file('cb_2013_us_zcta510_500k.shp')
-  # GDF.to_crs(GDF.crs, inplace=True)  
-  # plt.xticks(rotation=90)
-
-def PlotPoints(zip_code):
-  series = GDF[GDF[ZIP_HEADER] == zip_code]
-  series.plot()
 
 
+def ProcessRequest(departure, destination):
+  """Handles incoming web requests.
 
-if __name__ == '__main__':
-  # Usually, this file is imported as a module. However,
-  # it can be run from command line  interface as well.
-  CreateGeoDataFrame()
-  
-  # PlotPoints('94114')
-  # PlotPoints('08540')
+  Args:
+    departure: A tuple containing an x and y coordinate (x, y)
+    destination: A tuple containing an x and y coordinate (x, y)
 
-  # plt.savefig('nyc.png', dpi=DPI, bbox_inches='tight')
+  Returns:
+    A tuple containing the list of zip codes and the path to the
+    image generated.
+  """
+  # Create a shapely line that we will query GeoPandas with.
+  line = shapely.geometry.LineString([departure[::-1], destination[::-1]])
+  line_gdf = gp.GeoDataFrame([{'geometry': line}])
+  intersect_results = GDF['geometry'].intersects(line)
+  # Polygon coords for google maps.
+  polygon_coords = []
+  # A dataframe with only the resulting multipolygon objects to plot
+  gdf_results = []
+  # list of all zip codes to cross through
+  zips = []
+
+  for index, result in enumerate(intersect_results):
+    if result:
+      zips.append(GDF[ZIP_HEADER][index])
+      multi_polygon = GDF['geometry'][index]
+      gdf_results.append({'geometry': multi_polygon})
+      if isinstance(multi_polygon, shapely.geometry.polygon.Polygon):
+        polygon_coords.append(multi_polygon.exterior)
+      else:
+        # iterate through the multi polygon to get exterior
+        for polygon in multi_polygon:
+          polygon_coords.append(polygon.exterior)
+  results = gp.GeoDataFrame(gdf_results)
+  results.plot()  # plot all the zip code polygons.
+  line_gdf.plot()  # plot a dataframe with the line
+  image_path = '%s.png' % hash(departure + destination)
+  plt.savefig(image_path, bbox_inches='tight', dpi=DPI)
+  return zips, image_path
